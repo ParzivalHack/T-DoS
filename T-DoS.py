61bee6cc046e8e09869cf33a92519618deb37293
ParzivalKali <82817793+ParzivalHack@users.noreply.github.com>
2022-05-08 16:42:08 +0200

    Update T-DoS.py

--- T-DoS.py
+++ T-DoS.py
@@ -51,7 +51,7 @@ while True:
    b = str(random.randint(1,254))
    c = str(random.randint(1,254))
    d = str(random.randint(1,254))
-   dot = .
+   dot = (".")
    
    Source_ip = a + dot + b + dot + c + dot + d
    IP1 = IP(source_IP = source_IP, destination = target_IP)
