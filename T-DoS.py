1e6be0f62b1a6f2115ac6dc02f2cc2fc7860d51a
ParzivalKali <82817793+ParzivalHack@users.noreply.github.com>
2022-05-08 15:27:58 +0200

    Update T-DoS.py

--- T-DoS.py
+++ T-DoS.py
@@ -33,15 +33,15 @@ print("\033[93m")
 os.system("figlet DoS Attack")
 print("Coded by: ParzivalHack")
 print ("\033[92m")
-print "[                    ] 0% "
+print("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print("[===============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
