cab2cc69cdde3d00631fa7a2ee09786a8bf9ffcd
Tommaso Bona <piergeolo@gmail.com>
2022-12-02 10:50:31 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,19 +12,19 @@ exit 1
 fi
 cd $HOME
 sleep 1
-echo -e "         \e[91mUPDATE IS GOING ON, PLEASE BE PATIENT...!\e[0m"
+echo -e "         Updating T-DoS..."
 echo
 printf "                     \e[96m["
 # While process is running...
 while git clone https://github.com/ParzivalHack/T-DoS 2> /dev/null; do 
-    printf  "\e[91m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    printf  "▓▓▓▓▓▓▓▓▓▓▓▓▓"
     sleep 1
 done
-printf "\e[91m]\e[0m"
+printf " "
 echo
 echo
 echo
-printf "\e[91m           UPDATED SUCCESSFULLY TO THE LATEST VERSION!\e[0m"
+printf "           Successfully updated to the latest version!"
 sleep 2.0
 clear
 cd $HOME
