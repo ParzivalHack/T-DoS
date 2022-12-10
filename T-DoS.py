1f1eef99ae830309a9fdce64a01015c2632065fc
Tommaso Bona <piergeolo@gmail.com>
2022-12-10 19:31:30 +0100

    Update T-DoS.py

--- T-DoS.py
+++ T-DoS.py
@@ -61,7 +61,7 @@ def SYN_Flood(dstIP,dstPort,counter):
 	stdout.write("\nTotal packets sent: %i\n" % total)
 def Ping_sl():
 	ip = raw_input("Target IP: ")
-        ping_number = int(input("Number of pings (5000 suggested): "))
+        ping_number = raw_input("Number of pings (5000 suggested): "))
         for a in range(ping_number):
 		os.system("ping" + ping_number)
 
